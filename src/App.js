f57f12f9337a63546691866daf9cf8d512bca2f7
Elina <elinasterlitamak0810@mail.ru>
2023-11-03 16:41:55 +0300

    added react-calendar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 function App() {
+  const [date, setDate] = useState(new Date());
+
+  const onChange = (newDate) => {
+    setDate(newDate);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Учебный календарь</h1>
+      <Calendar
+        onChange={onChange}
+        value={date}
+      />
+      <p>Выбранная дата: {date.toLocaleDateString()}</p>
     </div>
   );
 }
