46cb2da9b699b618db73d5128827583619a42156
4215 Gallyamova Elina <elinasterlitamak0810@mail.ru>
2023-12-15 05:41:24 +0300

    Добавлен бэк плюс внесены изменения из-за этого

---
+++ DbConnection/App.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const app = express();
+const port = 5050; // Укажи подходящий порт
+const pgp = require('pg-promise')();
+
+// Замени connectionStr на свои реальные данные для подключения к PostgreSQL
+const connectionStr = 'postgresql://postgres:1111@localhost:5432/calendar.db';
+const db = pgp(connectionStr);
+
+app.get('/directions', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+
+    try {
+        const directions = await db.any('SELECT code, name FROM directions_data;');
+        res.json(directions);
+    } catch (error) {
+        console.error('Error fetching directions:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/study-forms', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+  
+    try {
+      const studyForms = await db.any('SELECT study_form_name FROM study_forms;');
+      res.json(studyForms);
+    } catch (error) {
+      console.error('Error fetching study forms:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/session_dates', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+  
+    try {
+      const numOfWeeks = await db.any('SELECT num_of_weeks FROM session_dates;');
+      res.json(numOfWeeks);
+    } catch (error) {
+      console.error('Error fetching number of weeks:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/summer_practice_dates', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+  
+    try {
+      const numOfWeeks = await db.any('SELECT num_of_weeks FROM summer_practice_dates;');
+      res.json(numOfWeeks);
+    } catch (error) {
+      console.error('Error fetching number of weeks:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/vacation_dates', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+  
+    try {
+      const numOfWeeks = await db.any('SELECT num_of_weeks FROM vacation_dates;');
+      res.json(numOfWeeks);
+    } catch (error) {
+      console.error('Error fetching number of weeks:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/terms', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+  
+    try {
+      const numOfTerm = await db.any('SELECT number_of_term FROM term_data;');
+      res.json(numOfTerm);
+    } catch (error) {
+      console.error('Error fetching number of weeks:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
--- src/src/App.js
+++ src/src/App.js
@@ -1,13 +1,94 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './App.css';
 
 function App() {
   const [date, setDate] = useState(new Date());
+  const [selectedTerm, setSelectedTerm] = useState('1'); // По умолчанию выбран первый семестр
+  const [selectedDirection, setSelectedDirection] = useState('09.03.03');
+  const [directions, setDirections] = useState([]);
+  const [studyForms, setStudyForms] = useState([]);
+  const [numOfSession, setNumOfSession] = useState(undefined);
+  const [numOfPractice, setNumOfPractice] = useState(undefined);
+  const [numOfVacation, setNumOfVacation] = useState(undefined);
+  const [term, setTerm] = useState([]);
 
   const onChange = (newDate) => {
     setDate(newDate);
+  };
+
+  useEffect(() => {
+    fetchStudyForms();
+    fetchTerm();
+    fetchDirections();
+  }, []);
+
+  const fetchDirections = () => {
+    fetch('http://localhost:5050/directions')
+      .then(response => response.json())
+      .then(data => setDirections(data.map(direction => direction.code)))
+      .catch(error => console.error('Error fetching directions:', error));
+  };
+
+  const fetchStudyForms = () => {
+    fetch('http://localhost:5050/study-forms')
+      .then(response => response.json())
+      .then(data => setStudyForms(data))
+      .catch(error => console.error('Error fetching study forms:', error));
+  };
+
+  const fetchNumOfSession = () => {
+    fetch('http://localhost:5050/session_dates')
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      setNumOfSession(json[0].num_of_weeks);
+    });
+
+    alert(numOfSession);
+  };
+
+  const fetchNumOfPractice = () => {
+    fetch('http://localhost:5050/summer_practice_dates')
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      setNumOfPractice(json[0].num_of_weeks);
+    });
+
+    alert(numOfPractice);
+  };
+
+  const fetchNumOfVacation = () => {
+    fetch('http://localhost:5050/vacation_dates')
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      setNumOfVacation(json[0].num_of_weeks);
+    });
+
+    alert(numOfVacation);
+  };
+
+  const fetchTerm = () => {
+    fetch('http://localhost:5050/terms')
+      .then(response => response.json())
+      .then(data => setTerm(data.map(term => term.number_of_term)))
+      .catch(error => console.error('Error fetching courses:', error));
+  };
+
+  const onTermChange = (event) => {
+    setSelectedTerm(event.target.value);
+    fetchTerm();
+  };
+
+  const onDirectionChange = (event) => {
+    setSelectedDirection(event.target.value);
+    fetchDirections();
   };
 
   return (
@@ -16,16 +97,32 @@ function App() {
         <div className='sidenav'>
           <div className='form-education'>
             <p>Форма обучения:</p>
-            <input type='radio' id='full-time' name='form' value='full-time' />
-              <label for='full-time'>Очная</label><br />
-            <input type='radio' id='part-time' name='form' value='part-time' />
-              <label for='part-time'>Очно-заочная</label><br />
-            <input type='radio' id='correspondence' name='form' value='correspondence' />
-              <label for='correspondence'>Заочная</label><br />
+            {studyForms.map((form) => (
+              <div key={form.study_form_name}>
+                <input type='radio' id={form.study_form_name} name='form' value={form.study_form_name} />
+                <label htmlFor={form.study_form_name}>{form.study_form_name}</label><br />
+          </div>
+        ))}
           </div>
           <div className='num-course'>
-            <p>Курс:</p>
-
+            <p>Семестр:</p>
+              <select value={selectedTerm} onChange={onTermChange}>
+                {term.map((term) => (
+                  <option key={term} value={term}>
+                    {term}
+                  </option>
+                ))}
+              </select>
+          </div>
+          <div className='direction-code'>
+            <p>Код направления:</p>
+            <select value={selectedDirection} onChange={onDirectionChange}>
+              {directions.map((direction) => (
+                <option key={direction} value={direction}>
+                  {direction}
+                </option>
+              ))}
+            </select>
           </div>
         </div>
       <div className="calendar-container">
@@ -37,6 +134,10 @@ function App() {
             selectRange={true}
           />
       </div>  
+
+      <button onClick={fetchNumOfSession}>
+        <span>fetch session</span>
+      </button>
     </div>
   );
 }
